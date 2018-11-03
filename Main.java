d911bf5f52f9b13b9c941b74eb7eac86fd04e780
Omm Mishra <41461196+ommmishra@users.noreply.github.com>
2018-11-03 20:38:15 +0530

    simplifying
    Simplified the whole program, deleted redundant lines of code.

--- Main.java
+++ Main.java
@@ -1,55 +1,49 @@
-
-
 import java.sql.*;
 import java.util.Scanner;
 
 class DatabaseCreation {
 
+    private Connection conn;
+    private Statement st;
+    private ResultSet rs;
+
+    public DatabaseCreation(Connection conn, Statement st){
+
+        this.conn = conn;
+        this.st = st;
+
+    }
+
     public void DatabaseCreate() {
-        Connection conn;
-        Statement st;
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "ultrablast2018");
-            st = conn.createStatement();
             String sql = "CREATE DATABASE notes;";
             st.executeUpdate(sql);
-
+            System.out.println("Created Database");
         } catch (Exception e) {
-            System.out.println("Something went wrong 1 " + e.getMessage());
+            System.out.println("Something went wrong in DatabaseCreate \n " + e.getMessage());
         }
     }
 
     public boolean checkDatabase() {
-        Connection conn;
-        Statement st;
-        ResultSet rs;
+
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "ultrablast2018");
-            st = conn.createStatement();
             String sql = "SHOW DATABASES;";
             rs = st.executeQuery(sql);
             while (rs.next()) {
                 if ("notes" == rs.getString(1)) {
+                    System.out.println("Database already exists");
                     return false;
                 }
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong 2 " + e.getMessage());
+            System.out.println("Something went wrong in checkDatabase \n " + e.getMessage());
         }
         return true;
     }
 
     public boolean CheckTable() {
-        Connection conn;
-        Statement st;
-        ResultSet rs;
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/notes", "root", "ultrablast2018");
-            st = conn.createStatement();
             String sql = "SHOW TABLES;";
             rs = st.executeQuery(sql);
             while (rs.next()) {
@@ -58,18 +52,15 @@ class DatabaseCreation {
                 }
             }
         } catch (Exception e) {
-            System.out.println("Something went wrong 3 " + e.getMessage());
+            System.out.println("Something went wrong in CheckTable \n " + e.getMessage());
         }
         return true;
     }
 
     public void CreateTable() {
-        Connection conn;
-        Statement st;
-        ResultSet rs;
+
+
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/notes", "root", "ultrablast2018");
             st = conn.createStatement();
             String sql = "USE notes;";
             st.executeUpdate(sql);
@@ -79,10 +70,31 @@ class DatabaseCreation {
 
 
         }catch (Exception e) {
-            System.out.println("Something went wrong 3 " + e.getMessage());
+            System.out.println("Something went wrong in CreateTable  \n" + e.getMessage());
         }
 
     }
+
+    public void queryDisplay(ResultSet rs){
+       try {
+
+
+           while (rs.next()) {
+               System.out.print(rs.getInt(1));
+               System.out.print(" ");
+               System.out.print(rs.getString(2));
+               System.out.print(" ");
+               System.out.print(rs.getString(3));
+               System.out.print(" ");
+               System.out.print(rs.getString(4));
+               System.out.println(" ");
+           }
+
+       }catch (Exception e){
+           System.out.println("Something went wrong in queryDisplay \n "+ e);
+       }
+
+       }
 
 }
 
@@ -98,21 +110,35 @@ class Main {
         Statement st;
         ResultSet rs;
 
+        try{
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "ultrablast2018");
+            System.out.println("Connected Database Successfully...\n\n");
+            st = conn.createStatement();
+        }catch (Exception e){
+           conn = null;
+           st = null;
+           System.out.println("Something went wrong \n" +e );
+        }
 
-        DatabaseCreation data = new DatabaseCreation();
-        if(data.checkDatabase()) {
-            data.DatabaseCreate();
+        DatabaseCreation data = new DatabaseCreation(conn, st);
+
+        try {
+
+            if (data.checkDatabase()) {
+                data.DatabaseCreate();
+            }
+
+            st.execute("USE notes;");
+        }catch (Exception e){
+            System.out.println("Something went wrong in this "+e);
         }
 
         if(data.CheckTable()){
-            data.CreateTable();
+         data.CreateTable();
         }
 
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/notes", "root", "ultrablast2018");
-            System.out.println("Connected Database Successfully...\n\n");
-            st = conn.createStatement();
+
             System.out.println("Enter 1 to proceed.");
             int a = SC.nextInt();
 
@@ -147,79 +173,49 @@ class Main {
                         o = SC.nextLine();
                         l = SC.nextInt();
                         switch (l){
+
                             case 1:
                                 sql = "SELECT * FROM reminder";
                                 rs = st.executeQuery(sql);
-
-                                while (rs.next()){
-                                    System.out.print(rs.getInt(1));
-                                    System.out.print(" ");
-                                    System.out.print(rs.getString(2));
-                                    System.out.print(" ");
-                                    System.out.print(rs.getString(3));
-                                    System.out.print(" ");
-                                    System.out.print(rs.getString(4));
-                                    System.out.println(" ");
-                                }
+                                data.queryDisplay(rs);
                                 break;
-                            case 2:
-                                System.out.println("Enter 1 to search reminder by No., Enter 2 to search reminder by reminder title, Enter 3 to search by date");
-                                l= SC.nextInt();
+
+                                case 2:
+                                    System.out.println("Enter 1 to search reminder by No., Enter 2 to search reminder by reminder title, Enter 3 to search by date");
+                                    l= SC.nextInt();
+
                                 switch (l){
+
                                     case 1:
                                         System.out.println("Enter the No. of reminder");
                                         o = SC.nextLine();
                                         l = SC.nextInt();
                                         sql = "SELECT * FROM reminder WHERE No = "+l+";";
                                         rs = st.executeQuery(sql);
-                                        while (rs.next()){
-                                            System.out.print(rs.getInt(1));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(2));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(3));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(4));
-                                            System.out.println(" ");
-                                        }
+                                        data.queryDisplay(rs);
                                         break;
 
                                     case 2:
+
                                         System.out.println("Enter the note of the reminder");
                                         j = SC.nextLine();
-                                         h = SC.nextLine();
+                                        h = SC.nextLine();
                                         sql = "SELECT * FROM reminder WHERE note = '"+h+"';";
                                         rs = st.executeQuery(sql);
-                                        while (rs.next()){
-                                            System.out.print(rs.getInt(1));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(2));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(3));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(4));
-                                            System.out.println(" ");
-                                        }
-                                            break;
+                                        data.queryDisplay(rs);
+                                        break;
 
                                     case 3:
+
                                         System.out.println("Enter the dates, between which you want to find the reminders.");
-                                         String s = SC.nextLine();
-                                         String n = SC.nextLine();
-                                         String t = SC.nextLine();
+                                        String s = SC.nextLine();
+                                        String n = SC.nextLine();
+                                        String t = SC.nextLine();
                                         sql = "SELECT * FROM reminder WHERE date BETWEEN '"+n+"' AND '"+t+"';";
                                         System.out.println(sql);
                                         rs = st.executeQuery(sql);
-                                        while (rs.next()){
-                                            System.out.print(rs.getInt(1));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(2));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(3));
-                                            System.out.print(" ");
-                                            System.out.print(rs.getString(4));
-                                            System.out.println(" ");
-                                        }
+                                        data.queryDisplay(rs);
+
                                         break;
 
                                 }
@@ -243,5 +239,4 @@ class Main {
 
 
 
-    }
-}
+    }}
